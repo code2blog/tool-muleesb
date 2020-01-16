f769647e07a9acc1ad1a3a4c13f27d5ac646891d
vishnu <vishnu31122013@gmail.com>
2020-01-16 12:31:07 +0530

    poll-1 two flows added. 1- code2blog_poll.xml - this polls a folder 2- global_elements.xml - this is a container for global configuration such as database config, http endpoints

---
+++ code2blog_poll/src/main/java/as/file_parse_to_db/FilePollingComponent.java
@@ -0,0 +1,35 @@
+package as.file_parse_to_db;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mule.api.MuleEventContext;
+import org.mule.api.MuleMessage;
+import org.mule.api.lifecycle.Callable;
+
+public class FilePollingComponent implements Callable {
+
+	@Override
+	public Object onCall(MuleEventContext eventContext) throws Exception {
+		File f = new File("D:\\vishnu\\workspace\\data\\muleesb\\as.file_parse_to_db-input\\");
+		List<File> filesToReturn = new ArrayList<File>(1);
+		if (f.isDirectory()) {
+			File[] files = f.listFiles();
+
+			for (File file : files) {
+				if (file.isFile())
+					filesToReturn.add(file);
+
+			}
+		} else {
+			throw new Exception("Invalid Directory");
+		}
+		MuleMessage message = eventContext.getMessage();
+		message.setPayload(filesToReturn);
+		message.setOutboundProperty("fileList", filesToReturn);
+
+		return filesToReturn;
+	}
+
+}
